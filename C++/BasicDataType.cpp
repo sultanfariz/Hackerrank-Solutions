d2f7cbe1a38e9fadf7c9a57dee388b978ec64b9b
Sultan Fariz <55394860+sultanfariz@users.noreply.github.com>
2020-06-30 16:44:25 +0700

    Create BasicDataType.cpp

---
+++ C++/BasicDataType.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a; long b; char c; float d; double e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<"\n"<<b<<"\n"<<c<<"\n";
+    printf("%.3lf\n",d); printf("%.9lf\n",e);
+
+    return 0;
+}
