9c33b00652fa78d22b8023e57dcdfe789c47478d
Sultan Fariz <55394860+sultanfariz@users.noreply.github.com>
2020-07-07 16:13:15 +0700

    Add files via upload

---
+++ 10 Days of Javascript/Day9-ButtonsContainer/buttonsGrid.js
@@ -0,0 +1,9 @@
+let btn5 = document.getElementById('btn5');
+
+let nums = [1, 2, 3, 6, 9, 8, 7, 4];
+const ids = [1, 2, 3, 6, 9, 8, 7, 4];
+
+btn5.addEventListener('click', function () {
+    nums.unshift(nums.pop());
+    for (i = 0; i <= 7; i++) document.getElementById("btn" + ids[i]).innerHTML = nums[i];
+});
