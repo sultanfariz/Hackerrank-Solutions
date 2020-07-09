d55fbda5edecf9c4e7836ea71fb44344f41a0f83
Sultan Fariz <55394860+sultanfariz@users.noreply.github.com>
2020-07-09 12:46:45 +0700

    Create OutputFormatting.java

---
+++ Java/OutputFormatting.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1=sc.next();
+                int x=sc.nextInt();
+                //Complete this line
+                int digit = 15-(s1.length());
+                System.out.print(s1);
+                for(int j=0; j<digit; j++) System.out.print(" ");
+                if(x<100 && x>9) System.out.println("0"+x);
+                else if(x<100 && x<10) System.out.println("00"+x);
+                else System.out.println(x);
+            }
+            System.out.println("================================");
+
+    }
+}
+
+
+
