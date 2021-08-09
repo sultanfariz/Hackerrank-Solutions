8417bca26a5483115ae1d8107a54b26079bd632a
Sultan Fariz <55394860+sultanfariz@users.noreply.github.com>
2021-08-09 11:12:19 +0700

    Create SalesByMerchant.go

---
+++ InterviewPreparationKit/Go/Warm-up Challenges/SalesByMerchant.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+    "sort"
+)
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+func sockMerchant(n int32, ar []int32) int32 {
+    // Write your code here
+    var queue int32 = 1
+    var pair int32 = 0
+    sort.Slice(ar, func(i, j int) bool { return ar[i] < ar[j] })
+    for i:=0; i<int(n-1); i++ {
+        if ar[i] == ar[i+1] {
+            queue++
+        } else {
+            pair += (queue / 2)
+            queue = 1
+        }
+        
+        if i == int(n-2) {
+            pair += (queue / 2)
+        }
+    }
+    return pair
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+    n := int32(nTemp)
+
+    arTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+    var ar []int32
+
+    for i := 0; i < int(n); i++ {
+        arItemTemp, err := strconv.ParseInt(arTemp[i], 10, 64)
+        checkError(err)
+        arItem := int32(arItemTemp)
+        ar = append(ar, arItem)
+    }
+
+    result := sockMerchant(n, ar)
+
+    fmt.Fprintf(writer, "%d\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
