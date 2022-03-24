5f7f2ea674c493b5820803cbe1fa080605ecaa07
Sultan Fariz <55394860+sultanfariz@users.noreply.github.com>
2022-03-24 10:24:10 +0700

    Create MigratoryBirds.go

---
+++ Algorithms/Go/MigratoryBirds.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+/*
+ * Complete the 'migratoryBirds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+func migratoryBirds(arr []int32) int32 {
+    // Write your code here
+    intmap := make(map[int32]int)
+    count, highest := 0, arr[0]
+    for _, val := range arr {
+        intmap[val]++
+    }
+    fmt.Println(intmap)
+    for idx, val := range intmap {
+        if count < val {
+            count = val
+            highest = idx
+        }
+    }
+    return highest
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    arrCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+
+    arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+    var arr []int32
+
+    for i := 0; i < int(arrCount); i++ {
+        arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+        checkError(err)
+        arrItem := int32(arrItemTemp)
+        arr = append(arr, arrItem)
+    }
+
+    result := migratoryBirds(arr)
+
+    fmt.Fprintf(writer, "%d\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
