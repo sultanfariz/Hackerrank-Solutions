a98ace4d3e56749cd4d0f767d68168185d17d965
Sultan Fariz <55394860+sultanfariz@users.noreply.github.com>
2022-07-06 11:40:44 +0700

    Create CamelCase.go

---
+++ Algorithms/Go/CamelCase.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strings"
+)
+
+/*
+ * Complete the 'camelcase' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+func camelcase(s string) int32 {
+    // Write your code here
+    count := 1
+    for _, char := range s {
+        if char < 95 {
+           count++ 
+        }
+    }
+    return int32(count)
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    s := readLine(reader)
+
+    result := camelcase(s)
+
+    fmt.Fprintf(writer, "%d\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
