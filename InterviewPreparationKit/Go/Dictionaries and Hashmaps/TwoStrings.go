5bfa33bdcfd6ced56ee404772f157dc464435f11
Sultan Fariz <55394860+sultanfariz@users.noreply.github.com>
2022-07-29 16:51:02 +0700

    Create TwoStrings.go

---
+++ InterviewPreparationKit/Go/Dictionaries and Hashmaps/TwoStrings.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+/*
+ * Complete the 'twoStrings' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. STRING s1
+ *  2. STRING s2
+ */
+
+func twoStrings(s1 string, s2 string) string {
+    // Write your code here
+    s1Map := make(map[rune]int)
+    for _, char := range s1 {
+        s1Map[char]++
+    }
+    for _, char := range s2 {
+        if s1Map[char] > 0 {
+            return "YES"
+        }
+    }
+    return "NO"
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    qTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+    q := int32(qTemp)
+
+    for qItr := 0; qItr < int(q); qItr++ {
+        s1 := readLine(reader)
+
+        s2 := readLine(reader)
+
+        result := twoStrings(s1, s2)
+
+        fmt.Fprintf(writer, "%s\n", result)
+    }
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
