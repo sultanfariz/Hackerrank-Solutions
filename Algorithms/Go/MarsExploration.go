af62946cae24c870cea8d2035e0d5372c16107eb
Sultan Fariz <55394860+sultanfariz@users.noreply.github.com>
2022-09-18 11:39:52 +0700

    Create MarsExploration.go

---
+++ Algorithms/Go/MarsExploration.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strings"
+)
+
+/*
+ * Complete the 'marsExploration' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+func marsExploration(s string) int32 {
+    // Write your code here
+    count := int32(0)
+    message := "SOS"
+    for i:=0; i<len(s); i++ {
+        if message[i%3] != s[i] {
+            count++
+        }
+    }
+    return count
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    s := readLine(reader)
+
+    result := marsExploration(s)
+
+    fmt.Fprintf(writer, "%d\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
