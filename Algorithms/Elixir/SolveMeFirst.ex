d01a554955e89a2b41b8b7c4618532643ac84f3d
Sultan Fariz <55394860+sultanfariz@users.noreply.github.com>
2023-03-31 13:32:58 +0700

    Create SolveMeFirst.ex

---
+++ Algorithms/Elixir/SolveMeFirst.ex
@@ -0,0 +1,11 @@
+defmodule Solution do
+  defp sum(a, b), do: a + b
+
+  def main() do
+    a = IO.gets("") |> String.trim |> String.to_integer
+    b = IO.gets("") |> String.trim |> String.to_integer
+    sum(a, b) |> IO.puts
+  end
+end
+
+Solution.main()
